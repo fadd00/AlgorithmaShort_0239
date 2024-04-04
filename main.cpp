8bd7aafbe733da7f1877794c1ad5533b417c0669
fadd00 <andhikahutama9@gmail.com>
2024-04-04 15:42:58 +0700

    step 6

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void LinearShort(){
     char ch;
     int ctr;
     int item;
-}
+
 do
 {
     cout << "\nEnter the element you want to search: "; // step 1
@@ -19,6 +19,16 @@ do
     ctr = 0;
     i = 0; //step 2
 } while (i < n);// step 3
+{
+    ctr++;
+    if (arr[i] == item)
+    {
+        cout << "\n " << item << "found at position " << (i+1) < endl; // step 6
+        break;
+    }
+    
+}
+}
 
 
 int main()
