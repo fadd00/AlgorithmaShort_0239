7a7d99f0bc807a6bc347c433a66f5aaa1f19327f
fadd00 <andhikahutama9@gmail.com>
2024-04-04 16:14:58 +0700

    input 1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,19 @@ int arr[20];
 int n;
 int i;
 void input(){
-
+    while (true)
+    {
+        cout << "enter the number of elementsin the array: ";
+        cin >> n;
+        if ((n > 0)&&(n <= 20))
+            break;
+         else
+            cout << "\nArray Should have minimum 1 and maximum 20 elements.\n\n";
+        
+        
+        
+    }
+    
 }
 void LinearSearch(){
     char ch;
