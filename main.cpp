af69b3ad6ed47de8950683a4a7568c804ba12784
fadd00 <andhikahutama9@gmail.com>
2024-04-04 16:18:09 +0700

    ipnut 2

--- main.cpp
+++ main.cpp
@@ -13,10 +13,15 @@ void input(){
         if ((n > 0)&&(n <= 20))
             break;
          else
-            cout << "\nArray Should have minimum 1 and maximum 20 elements.\n\n";
-        
-        
-        
+            cout << "\nArray Should have minimum 1 and maximum 20 elements.\n\n";   
+    }
+    cout << "\n---------------------\n";
+    cout << "Enter array elements\n";
+    cout << "--------------------\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << "> ";
+        cin >> arr[i];
     }
     
 }
